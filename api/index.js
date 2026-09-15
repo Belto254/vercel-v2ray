5262641ae99ae21b1657b2a1f1f15fa524839ea7
Belto254 <natsabel14@gmail.com>
2026-09-15 11:30:06 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,36 +13,30 @@ wss.on("connection", (client) => {
   );
 
   target.on("open", () => {
-    client.on("message", (data) => {
-      if (target.readyState === WebSocket.OPEN) {
-        target.send(data);
-      }
-    });
-  });
-
-  target.on("message", (data) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
+      client.send("BACKEND_CONNECTED");
     }
   });
 
-  target.on("error", () => {
-    client.close();
+  target.on("error", (err) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send("BACKEND_ERROR");
+      client.close();
+    }
   });
 
   target.on("close", () => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send("BACKEND_CLOSED");
+    }
     client.close();
-  });
-
-  client.on("close", () => {
-    target.close();
   });
 });
 
 app.get("/", (req, res) => {
   res.json({
     status: "online",
-    relay: "active"
+    relay: "diagnostic"
   });
 });
 
